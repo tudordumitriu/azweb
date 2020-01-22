853d850bccf6db182d3dad87423223fc3e54be80
Tudor Dumitriu <tudor.dumitriu@adaptabi.com>
2020-01-22 05:02:12 +0200

    Build test

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './style.scss';
 const Footer = () => (
   <footer>
     <section>This project is licensed under the MIT license.</section>
-    <section>Made by Dragos Tudor Dumitriu, with love :x</section>
+    <section>Made by Dragos Tudor Dumitriu, with love</section>
   </footer>
 );
 
