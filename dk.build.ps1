daabedcc637b9a52de25f9e707c230652921a6c5
Tudor Dumitriu <tudor.dumitriu@adaptabi.com>
2020-02-29 15:04:16 +0200

    Automated local build process

--- dk.build.ps1
+++ dk.build.ps1
@@ -1,8 +1,14 @@
 param (
-    [string]$version = 'v10'
+    [string]$version = 'v10',
+    [string]$apiUrl = 'http://localhost:30643/'
 )
 
 write-output "Building version: $version"
+
+write-output "Creating a backup of package.json"
+Get-Content .\package.json | Set-Content .\packageBack.json
+write-output "Rewriting the api url"
+(Get-Content .\package.json).replace('{apiurl}', $apiUrl) | Set-Content .\package.json
 
 $Command = "docker build -t tudordumitriu/azweb:$version ."
 write-output "Executing $Command"
@@ -12,9 +18,10 @@ $Command = "docker push tudordumitriu/azweb:$version"
 write-output "Executing $Command"
 Invoke-Expression $Command
 
-# write-output "Executing kubectl apply ..."
-# (Get-Content .\deployment.yaml).replace('{version}', $version) | Set-Content .\deploymentTemp.yaml
-# kubectl apply -f .\deploymentTemp.yaml
-# Remove-Item –path .\deploymentTemp.yaml
+write-output "Executing kubectl apply ..."
+(Get-Content .\deployment.yaml).replace('{version}', $version) | Set-Content .\deploymentTemp.yaml
+kubectl apply -f .\deploymentTemp.yaml
+Remove-Item –path .\deploymentTemp.yaml
 
-
+write-output "Restoring package.json"
+Get-Content .\packageBack.json | Set-Content .\package.json
